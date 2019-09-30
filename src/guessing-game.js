a64c07ea1aba4cf927c8f18c7e88e9a40a07c05c
redcliphaloe <redcliphaloe@gmail.com>
2019-09-30 17:01:23 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.answer =  Math.round((this.min + this.max) / 2);
+        return this.answer;
     }
 
     lower() {
-
+        this.max = this.answer;
     }
 
     greater() {
-
+        this.min = this.answer;
     }
 }
 
